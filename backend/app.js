fba01e9450fc6d17e06c466dab70f6a3afe410d2
henry walton <hnrywltn@gmail.com>
2021-07-15 09:50:24 -0700

    errorHandling

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 // backend/app.js
 const routes = require('./routes');
 
+//for catching sequelize errors
+const { ValidationError } = require('sequelize');
+
 
 
 const express = require('express');
@@ -50,9 +53,41 @@ app.use(
 
 
 
-
   app.use(routes); // Connect all the routes
 
+
+
+  // Catch unhandled requests and forward to error handler.
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+  });
+
+  // Process sequelize errors
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      err.errors = err.errors.map((e) => e.message);
+      err.title = 'Validation error';
+    }
+    next(err);
+  });
+
+  //formatting the errors into JSON and including the status code
+  // Error formatter
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack,
+    });
+  });
 
 
 
